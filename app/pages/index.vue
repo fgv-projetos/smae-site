1349c4260ff1359501286f67cf3f6ef5896b3147
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-09-05 16:06:51 -0300

    feat: configurando eslint projeto

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>index</h1>
+    <h2
+      class="123"
+      label="123"
+    >
+      index
+    </h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/eslint'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts']
-})
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: true,
+    },
+  },
+});
