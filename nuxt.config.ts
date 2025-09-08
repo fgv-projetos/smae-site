a79f980199d13c65e3da069101a762ae80adc120
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-09-08 17:07:28 -0300

    feat: added vue utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@vueuse/nuxt',
+  ],
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
   compatibilityDate: '2025-07-15',
