72d3f6bc3854fa78373cd7d89c7679b80a2d266e
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-09-12 13:22:20 -0300

    feat: adjusting import

--- app/pages/index/index.vue
+++ app/pages/index/index.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script lang="ts" setup>
-import About from './_sections/about.vue';
+import About from './_sections/About.vue';
 import Advantages from './_sections/Advantages.vue';
 import ContactUs from './_sections/ContactUs.vue';
 import Hero from './_sections/Hero.vue';
