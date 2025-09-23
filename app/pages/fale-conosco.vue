6067970a5bdaadd4b77714eca33e358c12ad9cd4
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-09-22 21:29:27 -0300

    feat: created contactus page

---
+++ app/pages/fale-conosco.vue
@@ -0,0 +1,96 @@
+<template>
+  <section class="contact-us">
+    <h1>Fale conosco</h1>
+
+    <div class="contact-us__container">
+      <form
+        class="max-section"
+        method="get"
+        @submit.prevent="handleSubmit"
+      >
+        <div class="form-group">
+          <FieldInput name="name" label="nome" />
+
+          <FieldInput name="email" label="E-mail" />
+        </div>
+
+        <div class="form-group">
+          <FieldInput name="subject" label="Assunto" />
+        </div>
+
+        <div class="form-group">
+          <FieldInput
+            name="description"
+            label="Descricao"
+            type="textarea"
+          />
+        </div>
+
+        <CtaButton
+          label="Enviar"
+          alternative
+          type="submit"
+        />
+      </form>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import CtaButton from '~/components/CtaButton.vue';
+import FieldInput from '~/components/FieldInput.vue';
+
+function handleSubmit(ev: SubmitEvent) {
+  if (!ev.target) {
+    return
+  }
+
+  const formData = new FormData(ev.target)
+  const data = {
+    name: formData.get('name'),
+    email: formData.get('email'),
+    subject: formData.get('subject'),
+    description: formData.get('description'),
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.contact-us {
+  h1 {
+    margin-top: 34px;
+    color: $primary-50;
+    text-align: center;
+    font-weight: 700;
+    font-size: 2.5rem;
+    line-height: 4.25rem;
+  }
+
+  form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 1.44rem;
+    padding: 50px 25px 70px;
+  }
+}
+
+.form-group {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.44rem;
+  flex: 1;
+}
+
+.contact-us__container {
+  background-color: $white;
+}
+
+@container (width > 1000px) {
+  .form-group {
+    flex-direction: row;
+    gap: 3.13rem;
+  }
+}
+</style>
