6499e8c2bbfba15ca48de32d79a6eaf850c9828a
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-11-10 12:39:36 -0300

    feat: moving contact form to main page

--- app/pages/index/_sections/ContactUs.vue
+++ app/pages/index/_sections/ContactUs.vue
@@ -20,7 +20,7 @@
           </p>
         </div>
 
-        <CtaButton label="Fale conosco" :to="{ name: 'fale-conosco' }" />
+        <CtaButton label="Fale conosco" :to="{ hash: '#formulario' }" />
       </aside>
     </article>
 
